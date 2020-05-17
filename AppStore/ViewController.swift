030bb932b3b704d54551daa649977a371f6cd0fe
Min <qwer810520@gmail.com>
2020-05-17 14:48:44 +0800

    Initialization

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Min on 2020/5/17.
+//  Copyright © 2020 Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
